a365862d37af5a8c4379dd4396900ae8f7286f21
= <=>
2016-04-27 21:24:09 -0500

    Now when app starts, a camera intent is sent and the image is displayed in the initial activity.

--- app/src/main/java/com/rowell/andrew/colorgetter/CameraActivity.java
+++ app/src/main/java/com/rowell/andrew/colorgetter/CameraActivity.java
@@ -1,13 +1,29 @@
 package com.rowell.andrew.colorgetter;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class CameraActivity extends AppCompatActivity {
+    private static final int CAMERA_REQUEST = 1888;
+    private ImageView imageView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+        this.imageView = (ImageView)this.findViewById(R.id.imageView);
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
     }
 }
